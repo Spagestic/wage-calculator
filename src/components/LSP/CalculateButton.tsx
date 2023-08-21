82362241620612cee9cb7f289d83bda7f9b17eed
Chaewon Kim <chaewonkim8@gmail.com>
2023-08-21 19:05:17 +0900

    component CalculateButton turned into a separate tsx file

---
+++ src/components/LSP/CalculateButton.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button, Tooltip } from '@mantine/core';
+import { motion } from 'framer-motion';
+
+interface CalculateButtonProps {
+  onClick: () => void;
+  disabled: boolean;
+  minYearsOfService: number;
+  yearsOfService: number;
+}
+
+const CalculateButton: React.FC<CalculateButtonProps> = ({ onClick, disabled, minYearsOfService, yearsOfService }) => {
+  return (
+    <Tooltip
+      withArrow
+      multiline
+      label={
+        !(yearsOfService >= minYearsOfService) 
+          ? `Requires ${minYearsOfService} years of work for long service payment!`
+          : "Monthly Wage should not be 0!"
+      }
+      disabled={!disabled}
+      events={{ hover: true, focus: true, touch: true }}
+    >
+      <motion.div whileTap={{ scale: 0.9 }}>
+        <Button
+          fullWidth
+          m="auto"
+          size="md"
+          radius="md"
+          onClick={onClick}
+          variant="gradient"
+          gradient={{ from: 'indigo', to: 'cyan' }}
+          disabled={disabled}
+        >
+          Calculate my payment
+        </Button>
+      </motion.div>
+    </Tooltip>
+  );
+};
+
+export default CalculateButton;
--- src/components/LSP/Input.tsx
+++ src/components/LSP/Input.tsx
@@ -1,25 +1,22 @@
 // Input.tsx
 import { useState, useEffect } from 'react';
-import {
-  NumberInput, Button, Box, Text, Tooltip,
-} from '@mantine/core';
-import { DatePickerInput } from '@mantine/dates';
-import { IconCalendar } from '@tabler/icons-react';
+import {Box, Text} from '@mantine/core';
 import Payment from './Payment';
 import WorkDateInput from './WorkDateInput';
 import MonthlyWageInput from './MonthlyWageInput';
+import CalculateButton from './CalculateButton';
 import { motion } from 'framer-motion';
 
 interface Props {
-    isVisible: boolean, 
-    handleOpen: () => void, 
+    isVisible: boolean,
+    handleOpen: () => void,
     handleClose: () => void,
-  }
+}
 
 const MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365.25;
 const MIN_YEARS_OF_SERVICE = 5;
 
-export default function Input({isVisible, handleOpen, handleClose}: Props) {
+export default function Input({ isVisible, handleOpen, handleClose }: Props) {
     const [startDate, setStartDate] = useState<Date | null>(null);
     const [endDate, setEndDate] = useState<Date | null>(null);
     const [wage, setWage] = useState<number | ''>(0);
@@ -27,110 +24,92 @@ export default function Input({isVisible, handleOpen, handleClose}: Props) {
 
     useEffect(() => {
         if (startDate && endDate) {
-          const serviceInMs = endDate.getTime() - startDate.getTime();
-          setYearsOfService(serviceInMs / MS_PER_YEAR);
+            const serviceInMs = endDate.getTime() - startDate.getTime();
+            setYearsOfService(serviceInMs / MS_PER_YEAR);
         }
-      }, [startDate, endDate]);
+    }, [startDate, endDate]);
 
     const canCalculatePayment = (yearsOfService >= MIN_YEARS_OF_SERVICE) && (wage !== ('' || 0));
 
-  return (
-    <>
-    {!isVisible && 
-    <motion.div 
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 0.3 }}
-    >
-        
-        <Box
-        p = "xl"
-        m = "lg"
-        mt = "xl"
-            sx={(theme) => ({
-                backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-                borderRadius: theme.radius.md,
-                // boxShadow: 'offset-x , offset-y , blur-radius , spread-radius , color' ,
-                boxShadow: '0px 0px 0px 0px rgba(0, 0, 0, 0.2)',
-            })}
-            >
-            <Text
-                align="left"
-                size="lg"
-                weight={250}
-                mt="xs"
-                mb="sm"
-                sx={(theme) => ({
-                    color: theme.colorScheme === 'dark' ? theme.colors.gray[4] : theme.colors.dark[5],
-                })}
-            >
-                Tell us your work dates
-            </Text>
-
-            <WorkDateInput 
-            label="Start working date" 
-            value={startDate} 
-            onChange={setStartDate} />
-
-            <WorkDateInput 
-            label="End working date" 
-            value={endDate} 
-            onChange={setEndDate} />
-
-            <Text
-                align="left"
-                size="lg"
-                weight={250}
-                mt="xl"
-                mb="sm"
-                sx={(theme) => ({
-                    color: theme.colorScheme === 'dark' ? theme.colors.gray[4] : theme.colors.dark[5],
-                })}
-            >
-                Tell us your last month's wage
-            </Text>
-            <MonthlyWageInput
-              value={wage}
-              onChange={setWage}
-            />
-            <Tooltip withArrow multiline
-            label={!(yearsOfService >= MIN_YEARS_OF_SERVICE) 
-                    ? `Requires ${MIN_YEARS_OF_SERVICE} years of work for long service payment!` 
-                    : "Monthly Wage should not be 0!"}
-            disabled={canCalculatePayment}
-            events={{ hover: true, focus: true, touch: true }}
-            >
+    return (
+        <>
+            {!isVisible &&
                 <motion.div
-                whileTap={{ scale: 0.9 }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.3 }}
                 >
-                    <Button
-                        fullWidth
-                        m="auto"
-                        size="md"
-                        radius="md"
-                        onClick={handleOpen}
-                        variant="gradient"
-                        gradient={{ from: 'indigo', to: 'cyan' }}
-                        disabled={!canCalculatePayment}
-                        >
-                        Calculate my payment
-                    </Button>
-                </motion.div>
-            </Tooltip>
-            </Box>
-            
 
-    </motion.div>}
-    
-    {isVisible && 
-        <Payment 
-        onClose={handleClose} 
-        wage={wage} 
-        yearsOfService={yearsOfService} 
-        startDate={startDate} 
-        endDate={endDate} />
-            }  
-    </>
-  );
+                    <Box
+                        p="xl"
+                        m="lg"
+                        mt="xl"
+                        sx={(theme) => ({
+                            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+                            borderRadius: theme.radius.md,
+                            // boxShadow: 'offset-x , offset-y , blur-radius , spread-radius , color' ,
+                            boxShadow: '0px 0px 0px 0px rgba(0, 0, 0, 0.2)',
+                        })}
+                    >
+                        <Text
+                            align="left"
+                            size="lg"
+                            weight={250}
+                            mt="xs"
+                            mb="sm"
+                            sx={(theme) => ({
+                                color: theme.colorScheme === 'dark' ? theme.colors.gray[4] : theme.colors.dark[5],
+                            })}
+                        >
+                            Tell us your work dates
+                        </Text>
+
+                        <WorkDateInput
+                            label="Start working date"
+                            value={startDate}
+                            onChange={setStartDate} />
+
+                        <WorkDateInput
+                            label="End working date"
+                            value={endDate}
+                            onChange={setEndDate} />
+
+                        <Text
+                            align="left"
+                            size="lg"
+                            weight={250}
+                            mt="xl"
+                            mb="sm"
+                            sx={(theme) => ({
+                                color: theme.colorScheme === 'dark' ? theme.colors.gray[4] : theme.colors.dark[5],
+                            })}
+                        >
+                            Tell us your last month's wage
+                        </Text>
+                        <MonthlyWageInput
+                            value={wage}
+                            onChange={setWage}
+                        />
+                        <CalculateButton
+                            onClick={handleOpen}
+                            disabled={!canCalculatePayment}
+                            minYearsOfService={MIN_YEARS_OF_SERVICE}
+                            yearsOfService={yearsOfService}
+                        />
+                    </Box>
+
+
+                </motion.div>}
+
+            {isVisible &&
+                <Payment
+                    onClose={handleClose}
+                    wage={wage}
+                    yearsOfService={yearsOfService}
+                    startDate={startDate}
+                    endDate={endDate} />
+            }
+        </>
+    );
 };
